6f85bf2bace50be655d6b7729c81923862c7375e
Vhasus <slicehost@thoughtworks.com>
2012-12-01 23:50:29 +0530

    [Suhas, Jaggu] Added class to generate MailBody

---
+++ MailBody.py
@@ -0,0 +1,17 @@
+class MailBody:
+	def __init__(self, header, csvrow):
+		self.header = header
+		self.csvrow = csvrow
+
+	def txt(self):
+		header_row = ""
+		value_row = ""
+		for i in range(0,len(self.header)):
+			header_row += self.text_with_tag(self.header[i], "th")
+			value_row += self.text_with_tag(self.csvrow[i], "td")
+		header_row = self.text_with_tag(header_row, "tr")
+		value_row = self.text_with_tag(value_row, "tr")
+		return self.text_with_tag(header_row + value_row, "table")
+
+	def text_with_tag(self, value, tag):
+		return "<%s>%s</%s>" % (tag, value, tag)
